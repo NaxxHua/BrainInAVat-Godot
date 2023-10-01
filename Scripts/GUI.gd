17b731155204ce24d91b80eae5bf1d24b37f9b80
Jiahua Ma <naxxhua@gmail.com>
2023-09-30 20:34:17 -0500

    feat: now GUI displays coin count

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -1,11 +1,9 @@
 extends CanvasLayer
 
-
 @onready var player = get_node("../Player")
 
 func _ready():
 	pass
 	
 func _physics_process(delta):
-	$coinText.text = String(player.coinCount)
-	
+	$coinText.text = str(player.coinCount)
